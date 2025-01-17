778c282b8cb3b935983e98bce2fa8882d14773e3
zero <dasenbrockjw@gmail.com>
2025-01-16 18:11:29 -0700

    Refactored chatgpt_convo module

--- zero_rag/chatgpt_convo.py
+++ zero_rag/chatgpt_convo.py
@@ -2,7 +2,6 @@
 
 import openai
 
-from .imports import * 
 from gpt_general.config import load_config
 
 CONFIG = load_config()
@@ -10,28 +9,28 @@ with open(CONFIG['api_key_path'], 'r') as f:
     openai.api_key = f.read().strip()
 
 
-def init_convo(message='you are a helpful assistant'):
+def init_convo(prompt='you are a helpful assistant'):
+    """Return an initial conversation object
+
+    The prompt param will be the first prompt received by ChatGPT. 
+
+    """
     conversation = [
-        {'role': 'system', 'content': message}
+        {'role': 'system', 'content': prompt}
     ]
     return conversation
 
 
-def new_message(message, conversation, model='', ):
+def new_prompt(prompt, conversation, model=''):
+    """Given a conversation/prompt, return an updated conversation"""
     if not model:
         raise Exception('must have model')
-    conversation.append({'role': 'user', 'content': message})
+    conversation.append({'role': 'user', 'content': prompt})
     response = openai.ChatCompletion.create(
         model=model,
         messages=conversation
     )
     reply = response['choices'][0]['message']['content']
     conversation.append({'role': 'assistant', 'content': reply})
-    d = {
-        'convo':str(conversation),
-        'message':message,
-        'reply':reply,
-    }
-    
-    return conversation, reply
 
+    return conversation, reply
